356cc7fead810173dc5d8faa356aaffc70185eb0
Gozebone <gozebone@gmail.com>
2022-08-18 23:03:13 +0300

    bind mousewheel on system componentDidMount

--- reactScripts/jsx/index.js
+++ reactScripts/jsx/index.js
@@ -23,6 +23,33 @@ class System extends React.Component {
             <Planet key={key} scale={scale} pos={pos}/>
         )
     }
+    componentDidMount() {
+        $(document).ready(function () {
+            $(document).bind('mousewheel', function (e) {
+                const tl = gsap.timeline();
+                var delta = e.originalEvent.wheelDelta;
+                if (delta > 0) {
+                    //go up
+                    tl.to(".planet", {
+                        scaleY: "*=" + scaleDelta,
+                        scaleX: "*=" + scaleDelta,
+                        y: "*=" + positionDelta,
+                        duration: 0
+                    })
+
+                } else {
+                    //go down
+                    tl.to(".planet", {
+                        scaleY: "/=" + scaleDelta,
+                        scaleX: "/=" + scaleDelta,
+                        y: "/=" + positionDelta,
+                        duration: 0
+                    })
+                }
+
+            });
+        });
+    }
 
     render() {
         return ([
