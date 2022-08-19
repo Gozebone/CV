6f7598aaba8a9239009b26c2711fff8dbbfec8d0
Gozebone <gozebone@gmail.com>
2022-08-19 03:28:22 +0300

    planet component as parameter in renderplanet

--- reactScripts/jsx/index.js
+++ reactScripts/jsx/index.js
@@ -3,11 +3,15 @@
 class Planet extends React.Component {
     constructor(props) {
         super(props);
+        // state = {
+        //     PlanetScale: 3,
+        //     PlanetPosition: 600,
+        // };
     }
 
     render() {
         return (
-            <SvgEat2EatPlanet style={{
+            <this.props.planet style={{
                 width: '1040', aspectRatio: '1', position: 'fixed',
                 left: '50%',  top: "0",
                 transform: `translate(-50%, ${this.props.pos}px) scale(${this.props.scale})`,
@@ -18,44 +22,53 @@ class Planet extends React.Component {
 }
 
 class System extends React.Component {
-    renderPlanet(key, scale, pos) {
+    constructor() {
+        super();
+
+    }
+
+    renderPlanet(planet, key, scale, pos) {
         return (
-            <Planet key={key} scale={scale} pos={pos}/>
+            <Planet planet={planet} key={key} scale={scale} pos={pos}/>
         )
     }
-    componentDidMount() {
-        $(document).ready(function () {
-            $(document).bind('mousewheel', function (e) {
-                const tl = gsap.timeline();
-                var delta = e.originalEvent.wheelDelta;
-                if (delta > 0) {
-                    //go up
-                    tl.to(".planet", {
-                        scaleY: "*=" + scaleDelta,
-                        scaleX: "*=" + scaleDelta,
-                        y: "*=" + positionDelta,
-                        duration: 0
-                    })
 
-                } else {
-                    //go down
-                    tl.to(".planet", {
-                        scaleY: "/=" + scaleDelta,
-                        scaleX: "/=" + scaleDelta,
-                        y: "/=" + positionDelta,
-                        duration: 0
-                    })
-                }
-
-            });
-        });
-    }
+    // componentDidMount() {
+    //     $(document).ready(function () {
+    //         $(document).bind('mousewheel', function (e) {
+    //             this.setState({PlanetScale: 3})
+    //         });
+    //     });
+    // }
+    //
+    // componentDidUpdate() {
+    //
+    //     var delta = e.originalEvent.wheelDelta;
+    //          if (delta > 0) {
+    //              //go up
+    //              tl.to(".planet", {
+    //                  scaleY: "*=" + scaleDelta,
+    //                  scaleX: "*=" + scaleDelta,
+    //                  y: "*=" + positionDelta,
+    //                  duration: 0
+    //              })
+    //
+    //          } else {
+    //              //go down
+    //              tl.to(".planet", {
+    //                  scaleY: "/=" + scaleDelta,
+    //                  scaleX: "/=" + scaleDelta,
+    //                  y: "/=" + positionDelta,
+    //                  duration: 0
+    //              })
+    //          }
+    // }
 
     render() {
         return ([
-                this.renderPlanet(1,3*(scaleDelta),600*(positionDelta)),
-                this.renderPlanet(2,3,600),
-                this.renderPlanet(3, 3/(scaleDelta),600/(positionDelta))
+                this.renderPlanet(SvgEat2EatPlanet, 1,3*(scaleDelta),600*(positionDelta)),
+                this.renderPlanet(SvgEat2EatPlanet, 2, 3,600),
+                this.renderPlanet(SvgEat2EatPlanet, 3, 3/(scaleDelta),600/(positionDelta))
             ]);
     }
 }
