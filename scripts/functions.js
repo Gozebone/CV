ec1f696c25d764fb7c1db3ac9bddbb6304133d80
Gozebone <gozebone@gmail.com>
2022-08-24 23:42:41 +0300

    last and first key in both increase and decrease functions

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,16 +1,17 @@
 const increaseKeys = (array) => {
-    let lastKey = Object.keys(array).pop()
-    for (let index = lastKey; index > lastKey - 5; index--){
-        array[index+1] = array[index]
+    let firstKey = Object.keys(array)[0], lastKey = Object.keys(array).pop()
+    for (let index = lastKey; index => firstKey; index--){
+        console.log(array)
+        array[parseInt(index)+1] = array[index]
         delete array[index]
     }
     return array
 }
 
 const decreaseKeys = (array) => {
-    let firstKey = Object.keys(array).pop()
-    for (let index = firstKey; index < lastKey + 5; index++){
-        array[index-1] = array[index]
+    let firstKey = Object.keys(array)[0], lastKey = Object.keys(array).pop()
+    for (let index = firstKey; index <= lastKey; index++){
+        array[parseInt(index)-1] = array[index]
         delete array[index]
     }
     return array
