7da26ff0ef456677c33af7a30baf59581da49a1d
Gozebone <gozebone@gmail.com>
2022-08-25 06:11:30 +0300

    scrollable on all positive mask numbers

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,17 +1,20 @@
 const increaseKeys = (array) => {
-    let firstKey = Object.keys(array)[0], lastKey = Object.keys(array).pop()
-    for (let index = lastKey; index => firstKey; index--){
-        console.log(array)
-        array[parseInt(index)+1] = array[index]
+    let firstKey = parseInt(Object.keys(array)[0]), lastKey = parseInt(Object.keys(array).pop())
+    for (let index = lastKey; index >= firstKey; index--){
+        array[index+1] = array[index]
         delete array[index]
     }
+
     return array
 }
 
 const decreaseKeys = (array) => {
-    let firstKey = Object.keys(array)[0], lastKey = Object.keys(array).pop()
+    let firstKey = parseInt(Object.keys(array)[0]), lastKey = parseInt(Object.keys(array).pop())
+    if (lastKey === 4) { // TODO make scrollable until 1 planet is visible
+        return array
+    }
     for (let index = firstKey; index <= lastKey; index++){
-        array[parseInt(index)-1] = array[index]
+        array[index-1] = array[index]
         delete array[index]
     }
     return array
